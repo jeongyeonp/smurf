e723356612a42150a02484503be8b660624940a0
jeongyeonp <okmari96@gmail.com>
2018-09-30 17:16:07 +0900

    2018/09/30 v0.1

---
+++ app/src/main/java/com/project/smurf/smurf/mypage.java
@@ -0,0 +1,29 @@
+package com.project.smurf.smurf;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class mypage extends AppCompatActivity {
+    private Button mp_al;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage);
+
+        mp_al = (Button)findViewById(R.id.mp_al);
+        mp_al.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), mypage_allergy.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+}
